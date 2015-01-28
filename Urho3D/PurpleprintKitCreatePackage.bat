69dbd950785ece27b13471df062c064476711a05
Hevedy <hgdavid@hotmail.es>
2015-01-28 22:18:46 +0100

    Urho3D update new materials & new models list

--- Urho3D/PurpleprintKitCreatePackage.bat
+++ Urho3D/PurpleprintKitCreatePackage.bat
@@ -1,5 +1,7 @@
 :: Purpleprint Kit - Create Package
 :: Convert and import 3D models
+
+:: Simple 3D Models
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Capsule.obj Data/PurpleprintKit/Models/Geometry/Simple/Capsule.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Cone.obj Data/PurpleprintKit/Models/Geometry/Simple/Cone.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Cube.obj Data/PurpleprintKit/Models/Geometry/Simple/Cube.mdl -b -na -nm -nt -nc -ns
@@ -11,6 +13,8 @@ AssetImporter.exe model ../../Content/3D/Geometry/Simple/Sphere.obj Data/Purplep
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Tetrahedron.obj Data/PurpleprintKit/Models/Geometry/Simple/Tetrahedron.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Torus.obj Data/PurpleprintKit/Models/Geometry/Simple/Torus.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/TorusKnot.obj Data/PurpleprintKit/Models/Geometry/Simple/TorusKnot.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/SphereCuboid.obj Data/PurpleprintKit/Models/Geometry/Simple/SphereCuboid.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/Hexagon.obj Data/PurpleprintKit/Models/Geometry/Simple/Hexagon.mdl -b -na -nm -nt -nc -ns
 
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/CapsuleInverted.obj Data/PurpleprintKit/Models/Geometry/Simple/Inverted/CapsuleInverted.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/ConeInverted.obj Data/PurpleprintKit/Models/Geometry/Simple/Inverted/ConeInverted.mdl -b -na -nm -nt -nc -ns
@@ -23,6 +27,8 @@ AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/SphereInverted
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/TetrahedronInverted.obj Data/PurpleprintKit/Models/Geometry/Simple/Inverted/TetrahedronInverted.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/TorusInverted.obj Data/PurpleprintKit/Models/Geometry/Simple/Inverted/TorusInverted.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/TorusKnotInverted.obj Data/PurpleprintKit/Models/Geometry/Simple/Inverted/TorusKnotInverted.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/SphereCuboidInverted.obj Data/PurpleprintKit/Models/Geometry/Simple/Inverted/SphereCuboidInverted.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/Inverted/HexagonInverted.obj Data/PurpleprintKit/Models/Geometry/Simple/Inverted/HexagonInverted.mdl -b -na -nm -nt -nc -ns
 
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/CapsuleShape.obj Data/PurpleprintKit/Models/Geometry/Simple/Shape/CapsuleShape.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/ConeShape.obj Data/PurpleprintKit/Models/Geometry/Simple/Shape/ConeShape.mdl -b -na -nm -nt -nc -ns
@@ -35,6 +41,8 @@ AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/SphereShape.obj D
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/TetrahedronShape.obj Data/PurpleprintKit/Models/Geometry/Simple/Shape/TetrahedronShape.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/TorusShape.obj Data/PurpleprintKit/Models/Geometry/Simple/Shape/TorusShape.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/TorusKnotShape.obj Data/PurpleprintKit/Models/Geometry/Simple/Shape/TorusKnotShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/SphereCuboidShape.obj Data/PurpleprintKit/Models/Geometry/Simple/Shape/SphereCuboidShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/Shape/HexagonShape.obj Data/PurpleprintKit/Models/Geometry/Simple/Shape/HexagonShape.mdl -b -na -nm -nt -nc -ns
 
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/CapsuleWireframe.obj Data/PurpleprintKit/Models/Geometry/Simple/Wireframe/CapsuleWireframe.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/ConeWireframe.obj Data/PurpleprintKit/Models/Geometry/Simple/Wireframe/ConeWireframe.mdl -b -na -nm -nt -nc -ns
@@ -47,6 +55,15 @@ AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/SphereWirefra
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/TetrahedronWireframe.obj Data/PurpleprintKit/Models/Geometry/Simple/Wireframe/TetrahedronWireframe.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/TorusWireframe.obj Data/PurpleprintKit/Models/Geometry/Simple/Wireframe/TorusWireframe.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/TorusKnotWireframe.obj Data/PurpleprintKit/Models/Geometry/Simple/Wireframe/TorusKnotWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/SphereCuboidWireframe.obj Data/PurpleprintKit/Models/Geometry/Simple/Wireframe/SphereCuboidWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Simple/Wireframe/HexagonWireframe.obj Data/PurpleprintKit/Models/Geometry/Simple/Wireframe/HexagonWireframe.mdl -b -na -nm -nt -nc -ns
+
+:: Tool 3D Models
+AssetImporter.exe model ../../Content/3D/Tools/References/Lightball.obj Data/PurpleprintKit/Models/Tools/References/Lightball.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Tools/References/MaterialCube.obj Data/PurpleprintKit/Models/Tools/References/MaterialCube.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Tools/References/MaterialExhibitor.obj Data/PurpleprintKit/Models/Tools/References/MaterialExhibitor.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Tools/References/MaterialFacesCube.obj Data/PurpleprintKit/Models/Tools/References/MaterialFacesCube.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Tools/References/MaterialFacesSphereCuboid.obj Data/PurpleprintKit/Models/Tools/References/MaterialFacesSphereCuboid.mdl -b -na -nm -nt -nc -ns
 
 AssetImporter.exe model ../../Content/3D/Tools/References/Cube_50x50.obj Data/PurpleprintKit/Models/Tools/References/Cube_50x50.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Tools/References/Cube_50x100.obj Data/PurpleprintKit/Models/Tools/References/Cube_50x100.mdl -b -na -nm -nt -nc -ns
@@ -54,6 +71,43 @@ AssetImporter.exe model ../../Content/3D/Tools/References/Cube_50x150.obj Data/P
 AssetImporter.exe model ../../Content/3D/Tools/References/Cube_50x200.obj Data/PurpleprintKit/Models/Tools/References/Cube_50x200.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Tools/References/Cube_50x250.obj Data/PurpleprintKit/Models/Tools/References/Cube_50x250.mdl -b -na -nm -nt -nc -ns
 AssetImporter.exe model ../../Content/3D/Tools/References/Cube_50x300.obj Data/PurpleprintKit/Models/Tools/References/Cube_50x300.mdl -b -na -nm -nt -nc -ns
+
+:: Advanced 3D Models
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/CubeHollow2Sides.obj Data/PurpleprintKit/Models/Geometry/Advanced/CubeHollow2Sides.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/CubeHollowOpen.obj Data/PurpleprintKit/Models/Geometry/Advanced/CubeHollowOpen.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/CylinderHollow2Sides.obj Data/PurpleprintKit/Models/Geometry/Advanced/CylinderHollow2Sides.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/CylinderHollowOpen.obj Data/PurpleprintKit/Models/Geometry/Advanced/CylinderHollowOpen.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/DecagonPlane.obj Data/PurpleprintKit/Models/Geometry/Advanced/DecagonPlane.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/HexagonPlane.obj Data/PurpleprintKit/Models/Geometry/Advanced/HexagonPlane.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/OctagonPlane.obj Data/PurpleprintKit/Models/Geometry/Advanced/OctagonPlane.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/PentagonPlane.obj Data/PurpleprintKit/Models/Geometry/Advanced/PentagonPlane.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/TrianglePlane.obj Data/PurpleprintKit/Models/Geometry/Advanced/TrianglePlane.mdl -b -na -nm -nt -nc -ns
+
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Inverted/CubeHollow2SidesInverted.obj Data/PurpleprintKit/Models/Geometry/Advanced/Inverted/CubeHollow2SidesInverted.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Inverted/CubeHollowOpenInverted.obj Data/PurpleprintKit/Models/Geometry/Advanced/Inverted/CubeHollowOpenInverted.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Inverted/CylinderHollow2SidesInverted.obj Data/PurpleprintKit/Models/Geometry/Advanced/Inverted/CylinderHollow2SidesInverted.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Inverted/CylinderHollowOpenInverted.obj Data/PurpleprintKit/Models/Geometry/Advanced/Inverted/CylinderHollowOpenInverted.mdl -b -na -nm -nt -nc -ns
+
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/CubeHollow2SidesShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/CubeHollow2SidesShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/CubeHollowOpenShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/CubeHollowOpenShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/CylinderHollow2SidesShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/CylinderHollow2SidesShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/CylinderHollowOpenShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/CylinderHollowOpenShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/DecagonPlaneShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/DecagonPlaneShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/HexagonPlaneShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/HexagonPlaneShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/OctagonPlaneShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/OctagonPlaneShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/PentagonPlaneShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/PentagonPlaneShape.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Shape/TrianglePlaneShape.obj Data/PurpleprintKit/Models/Geometry/Advanced/Shape/TrianglePlaneShape.mdl -b -na -nm -nt -nc -ns
+
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/CubeHollow2SidesWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/CubeHollow2SidesWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/CubeHollowOpenWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/CubeHollowOpenWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/CylinderHollow2SidesWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/CylinderHollow2SidesWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/CylinderHollowOpenWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/CylinderHollowOpenWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/DecagonPlaneWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/DecagonPlaneWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/HexagonPlaneWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/HexagonPlaneWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/OctagonPlaneWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/OctagonPlaneWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/PentagonPlaneWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/PentagonPlaneWireframe.mdl -b -na -nm -nt -nc -ns
+AssetImporter.exe model ../../Content/3D/Geometry/Advanced/Wireframe/TrianglePlaneWireframe.obj Data/PurpleprintKit/Models/Geometry/Advanced/Wireframe/TrianglePlaneWireframe.mdl -b -na -nm -nt -nc -ns
+
 :: Import 2D textures
 
 pause
